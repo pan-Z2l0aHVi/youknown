5b3037c18359d7e99cfb98ccb2b284fdd1771f50
Grey <1350210100@qq.com>
2023-11-08 10:59:56 +0800

    build: deploy sh

---
+++ apps/knowledge-fe/scripts/deploy.sh
@@ -0,0 +1,7 @@
+cd /Users/bopan/AWS
+sudo scp -r -i pan.pem /Users/bopan/Code/frontend/youknown/apps/knowledge-fe/dist/index.html ubuntu@ec2-16-163-157-253.ap-east-1.compute.amazonaws.com:/home/ubuntu/configure/fe/knowledge-fe
+
+border="====================================="
+echo "$border"
+echo "               部署完成"
+echo "$border"
