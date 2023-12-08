cb210f9b4544583f2af976eec9b1c3fc39aaf018
Grey <1350210100@qq.com>
2023-12-08 10:15:30 +0800

    build: split chunk

--- apps/react-demos/vite.config.ts
+++ apps/react-demos/vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
 import Unocss from 'unocss/vite'
-import { defineConfig, loadEnv, PluginOption } from 'vite'
+import { defineConfig, loadEnv, PluginOption, splitVendorChunkPlugin } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import react from '@vitejs/plugin-react-swc'
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd())
 	return {
 		base: '',
-		plugins: [tsconfigPaths(), react(), Unocss(), visualizer() as PluginOption],
+		plugins: [splitVendorChunkPlugin(), tsconfigPaths(), react(), Unocss(), visualizer() as PluginOption],
 		optimizeDeps: {
 			exclude: ['@jsquash/jpeg', '@jsquash/webp', '@jsquash/png', '@jsquash/oxipng']
 		},
