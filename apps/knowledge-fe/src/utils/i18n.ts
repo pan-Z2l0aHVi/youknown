80a2d49ce25754b283e95a3d3cb83aa4feaab24a
Grey <1350210100@qq.com>
2024-01-03 11:25:27 +0800

    feat: load i18next async

--- apps/knowledge-fe/src/utils/i18n.ts
+++ apps/knowledge-fe/src/utils/i18n.ts
@@ -1,47 +1,48 @@
 import resources_to_backend from 'i18next-resources-to-backend'
 import { initReactI18next } from 'react-i18next'
 
-const { default: i18next } = await import('i18next')
-
-export const change_lang = (...args: Parameters<typeof i18next.changeLanguage>) => {
-	i18next.changeLanguage(...args)
+export const change_lang = async (...args: Parameters<typeof import('i18next').changeLanguage>) => {
+	const { changeLanguage } = await import('i18next')
+	await changeLanguage(...args)
 }
 
-i18next
-	.use(initReactI18next)
-	.use(
-		resources_to_backend(async (language: string) => {
-			if (language === 'dev') {
-				return
-			}
-			// Why not "locales/${language}.json"?
-			// Vite 拼接动态引入monorepo依赖会报错，暂时用不那么优雅的方式处理
-			let deps_pkgs: Promise<unknown>[] = []
-			if (language === 'en') {
-				deps_pkgs = [
-					import('@youknown/react-ui/src/locales/en.json').then(res => res.default),
-					import('@youknown/react-rte/src/locales/en.json').then(res => res.default)
-				]
-			} else if (language === 'zh') {
-				deps_pkgs = [
-					import('@youknown/react-ui/src/locales/zh.json').then(res => res.default),
-					import('@youknown/react-rte/src/locales/zh.json').then(res => res.default)
-				]
-			}
-			const pkgs = await Promise.all([
-				import(`@/locales/${language}.json`).then(res => res.default),
-				...deps_pkgs
-			])
-			return pkgs.reduce(
-				(pre, cur) => ({
-					...pre,
-					...cur
-				}),
-				{}
-			)
+import('i18next').then(res => {
+	res.default
+		.use(initReactI18next)
+		.use(
+			resources_to_backend(async (language: string) => {
+				if (language === 'dev') {
+					return
+				}
+				// Why not "locales/${language}.json"?
+				// Vite 拼接动态引入monorepo依赖会报错，暂时用不那么优雅的方式处理
+				let deps_pkgs: Promise<unknown>[] = []
+				if (language === 'en') {
+					deps_pkgs = [
+						import('@youknown/react-ui/src/locales/en.json').then(res => res.default),
+						import('@youknown/react-rte/src/locales/en.json').then(res => res.default)
+					]
+				} else if (language === 'zh') {
+					deps_pkgs = [
+						import('@youknown/react-ui/src/locales/zh.json').then(res => res.default),
+						import('@youknown/react-rte/src/locales/zh.json').then(res => res.default)
+					]
+				}
+				const pkgs = await Promise.all([
+					import(`@/locales/${language}.json`).then(res => res.default),
+					...deps_pkgs
+				])
+				return pkgs.reduce(
+					(pre, cur) => ({
+						...pre,
+						...cur
+					}),
+					{}
+				)
+			})
+		)
+		.init({
+			fallbackLng: 'zh',
+			debug: import.meta.env.MODE === 'development'
 		})
-	)
-	.init({
-		fallbackLng: 'zh',
-		debug: import.meta.env.MODE === 'development'
-	})
+})
