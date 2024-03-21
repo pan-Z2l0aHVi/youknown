73ca7d49b0a424e022d7f2b3e7f36cd1599a82a6
Grey <1350210100@qq.com>
2024-03-21 23:32:26 +0800

    build: remove configure tracking

--- apps/knowledge-fe/scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-cd /Users/bopan/AWS
-# sudo scp -r -i pan.pem /Users/bopan/Code/frontend/youknown/apps/knowledge-fe/dist/assets ubuntu@ec2-16-163-30-187.ap-east-1.compute.amazonaws.com:/home/ubuntu/configure/fe/knowledge-fe/
-# Exclude assets
-sudo find /Users/bopan/Code/frontend/youknown/apps/knowledge-fe/dist -maxdepth 1 -type f -exec sh -c 'scp -i pan.pem "$@" ubuntu@ec2-16-163-30-187.ap-east-1.compute.amazonaws.com:/home/ubuntu/configure/fe/knowledge-fe/' sh {} +
-
-border="====================================="
-echo "$border"
-echo "               部署完成"
-echo "$border"
